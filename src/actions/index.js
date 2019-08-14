86416f3dafdc779c4accaebe7a3e1bb7f2a11311
Leana <leananeparidze.com>
2019-08-14 15:32:22 -0700

    refactors to use redux and completes all the functionalities to add and remove cars with price updates

---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+
+export const addFeature = feature => {
+  return { 
+    type: 'ADD_FEATURE', 
+    payload: feature 
+  };
+};
+
+export const deleteFeature = feature => {
+  return {
+    type: 'DELETE_FEATURE',
+    payload: feature
+  };
+};
